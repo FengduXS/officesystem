47f774607ffb6fc6ab7536c2caf815f293db130d
FengduXS <2418869820@qq.com>
2020-09-29 00:21:18 +0800

    xs

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,6 @@ const router = new VueRouter({
   routes
 })
 router.beforeEach((to, from, next) => {
-  store.dispatch("fdsafdas",routes)
   store.commit("changeSelectedMoudle",routes.filter(item => item.meta.title == to.meta.moduleName)[0])
   store.commit("changeSelectdMenu",to.meta.title)
   next()
